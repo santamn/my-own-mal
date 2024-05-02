211a499a3b423599c5bd310e1632285ac6bc42a1
miyake-yuki <cle.neige@gmail.com>
2024-05-02 21:57:53 +0900

    do, if, letを末尾再帰化可能に修正

--- impls/rust/src/bin/step5_2.rs
+++ impls/rust/src/bin/step5_2.rs
@@ -1,11 +1,15 @@
+use itertools::Itertools;
 use rustymal::env::Env;
-use rustymal::types::{MalResult, MalVal};
+use rustymal::printer;
+use rustymal::types::{Arity, Closure, MalError, MalResult, MalVal};
 
 fn main() {}
 
 #[allow(non_snake_case)]
 fn EVAL(input: MalVal, env: &mut Env) -> MalResult {
-    loop {}
+    loop {
+        todo!()
+    }
 }
 
 fn special_do(mut list: Vec<MalVal>, env: &mut Env) -> MalResult {
@@ -17,7 +21,7 @@ fn special_do(mut list: Vec<MalVal>, env: &mut Env) -> MalResult {
     Ok(last)
 }
 
-fn special_if(mut list: Vec<MalVal>, env: &mut Env) -> MalResult {
+fn special_if(list: &[MalVal], env: &mut Env) -> MalResult {
     if list.len() < 3 || list.len() > 4 {
         return Err(MalError::WrongArity(
             "if".to_string(),
@@ -26,14 +30,55 @@ fn special_if(mut list: Vec<MalVal>, env: &mut Env) -> MalResult {
         ));
     }
 
-    match EVAL(list.first().unwrap(), env)? {
+    match EVAL(list[1].clone(), env)? {
         MalVal::Bool(false) | MalVal::Nil => {
             if list.len() == 4 {
-                EVAL(&list[3], env)
+                Ok(list[3].clone())
             } else {
                 Ok(MalVal::Nil)
             }
         }
-        _ => EVAL(&list[2], env),
+        _ => Ok(list[2].clone()),
+    }
+}
+
+fn special_let(list: Vec<MalVal>, env: &mut Env) -> MalResult {
+    if list.len() != 3 {
+        return Err(MalError::WrongArity(
+            "let*".to_string(),
+            Arity::Fixed(2),
+            list.len() - 1,
+        ));
+    }
+
+    let [_, bindings, body] = unsafe { list.try_into().unwrap_unchecked() };
+    if let MalVal::List(bindings, _) | MalVal::Vector(bindings, _) = bindings {
+        let mut new_env = Env::new(Some(env));
+        bindings
+            .iter()
+            .chain(std::iter::once(&MalVal::Nil)) // 奇数個の場合に対応するため
+            .tuples()
+            .try_for_each(|(k, v)| {
+                if let MalVal::Symbol(s) = k {
+                    let val = EVAL(v.clone(), &mut new_env)?;
+                    new_env.set(s.to_string(), val);
+                    Ok(())
+                } else {
+                    Err(MalError::InvalidType(
+                        printer::pr_str(k, true),
+                        "symbol".to_string(),
+                        k.type_str(),
+                    ))
+                }
+            })?;
+
+        *env = new_env;
+        Ok(body)
+    } else {
+        Err(MalError::InvalidType(
+            printer::pr_str(&bindings, true),
+            "list or vec".to_string(),
+            bindings.type_str(),
+        ))
     }
 }
